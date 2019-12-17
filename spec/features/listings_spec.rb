a4175a205aeb1189cfff0b62e11fe4a87756c0d3
Jamie95187 <jamiewong63@googlemail.com>
2019-12-17 16:38:19 +0000

    create migration for avaialability table. add availability dates for listings

--- spec/features/listings_spec.rb
+++ spec/features/listings_spec.rb
@@ -7,14 +7,17 @@ feature 'create and view listings' do
     expect(current_path).to eq('/listings/new')
   end
 
-  scenario 'user can create and view their listing' do
+  scenario 'user can create and view their listing and show the available dates to rent out' do
     visit '/listings/new'
     fill_in 'listing_name', with: 'Test Listing 1'
     fill_in 'listing_description', with: 'i am a test description of test listing 1'
+    fill_in 'start_date', with: '01/01/2020'
+    fill_in 'end_date', with: '07/01/2020'
     click_button 'post_listing'
 
     expect(current_path).to eq('/listings')
     expect(page).to have_content("Test Listing 1", "i am a test description of test listing 1")
+    expect(page).to have_content("01/01/2020 - 07/01/2020")
   end
 
 end
