7e5237f428ef7c4435152110846cc106fe300f29
Alastair Gilles <agilles91@gmail.com>
2019-12-17 17:47:10 +0100

    add prices to listings and refactor helpers

--- app.rb
+++ app.rb
@@ -24,6 +24,7 @@ class MareBnB < Sinatra::Base
     Listing.create(
       name: params[:name],
       description: params[:description],
+      price: params[:price],
       user_id: session[:user_id]
     )
     redirect '/listings'
--- spec/features/listings_spec.rb
+++ spec/features/listings_spec.rb
@@ -11,10 +11,13 @@ feature 'create and view listings' do
     visit '/listings/new'
     fill_in 'listing_name', with: 'Test Listing 1'
     fill_in 'listing_description', with: 'i am a test description of test listing 1'
+    fill_in 'listing_price', with: 1000
     click_button 'post_listing'
 
     expect(current_path).to eq('/listings')
-    expect(page).to have_content("Test Listing 1", "i am a test description of test listing 1")
+    expect(page).to have_content("Test Listing 1")
+    expect(page).to have_content("i am a test description of test listing 1")
+    expect(page).to have_content(1000)
   end
 
 end
