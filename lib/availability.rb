9d1a4ca42136af5463a8c16ee8f970377e13fbc2
Hyan18 <Hyan18@github.com>
2019-12-18 16:19:40 +0000

    implement confirm_request

--- lib/availability.rb
+++ lib/availability.rb
@@ -1,7 +1,7 @@
 class Availability < ActiveRecord::Base
   def self.available?(start_date:, end_date:, listing_id:)
-    start_date = Date.parse(start_date)
-    end_date = Date.parse(end_date)
+    start_date = check_date_class(start_date)
+    end_date = check_date_class(end_date)
     raise AvailabilityError.new("Cannot have start date after end date") if start_date > end_date
     raise AvailabilityError.new("Must stay for at least one night") if start_date == end_date
 
@@ -13,6 +13,57 @@ class Availability < ActiveRecord::Base
 
   def self.select_by_listing(id)
     Availability.all.select { |avail| avail.listing_id == id }
+  end
+
+  def self.confirm_request(request, request_class = Request)
+    raise AvailabilityError.new("Cannot confirm rejected request") if request.rejected
+
+    unless available?(start_date: request.start,
+                      end_date: request.end,
+                      listing_id: request.listing_id)
+      raise AvailabilityError.new("Cannot confirm unavailable request")
+    end
+
+    request_class.update(request.id, accepted: true)
+
+    fix_availability(request)
+
+    requests = request_class.coinciding_requests(request)
+    requests.each { |req| request_class.update(req.id, rejected: true) }
+  end
+
+  def self.find_availability_for_request(request)
+    Availability.all.select do |avail|
+      avail.listing_id == request.listing_id &&
+      avail.start <= request.start &&
+      avail.end >= request.end
+    end.first
+  end
+
+  class << self
+
+    private
+
+    def fix_availability(request)
+      availability = find_availability_for_request(request)
+
+      if availability.start == request.start && availability.end == request.end
+        Availability.delete(availability.id)
+      elsif availability.start == request.start
+        Availability.update(availability.id, start: request.end)
+      elsif availability.end == request.end
+        Availability.update(availability.id, end: request.start)
+      else
+        Availability.update(availability.id, end: request.start)
+        Availability.create(start: request.end,
+                            end: availability.end,
+                            listing_id: request.listing_id)
+      end
+    end
+
+    def check_date_class(date)
+      Date.parse(date) if date.class == String
+    end
   end
 end
 
--- spec/units/availability_spec.rb
+++ spec/units/availability_spec.rb
@@ -1,49 +1,138 @@
 describe Availability do
+
+  let(:request_class) { double(:Request, update: request, coinciding_requests: [request, request, request]) }
+  let(:listing_id) { 1 }
+
+  before(:each) do
+    Availability.create(start: "01/01/2021", end: "31/01/2021", listing_id: listing_id)
+  end
+
   describe ".available?" do
-    before(:each) do
-      Availability.create(start: "01/01/2021", end: "31/01/2021", listing_id: 1)
-    end
 
     it "should return true if dates are available" do
       expect(Availability.available?(start_date: "02/01/2021",
                                      end_date: "05/01/2021",
-                                     listing_id: 1))
+                                     listing_id: listing_id))
         .to be true
     end
 
     it "should return false if dates are unavailable" do
       expect(Availability.available?(start_date: "02/02/2021",
                                      end_date: "05/02/2021",
-                                     listing_id: 1))
+                                     listing_id: listing_id))
         .to be false
     end
 
     it "should return true if dates are available" do
       expect(Availability.available?(start_date: "01/01/2021",
                                      end_date: "05/01/2021",
-                                     listing_id: 1))
+                                     listing_id: listing_id))
         .to be true
     end
 
     it "should return true if dates are available" do
       expect(Availability.available?(start_date: "01/01/2021",
                                      end_date: "31/01/2021",
-                                     listing_id: 1))
+                                     listing_id: listing_id))
         .to be true
     end
 
     it "should raise an error if the start is after the end date" do
       expect { Availability.available?(start_date: "05/01/2021",
                                      end_date: "03/01/2021",
-                                     listing_id: 1) 
+                                     listing_id: listing_id) 
       }.to raise_error(AvailabilityError, "Cannot have start date after end date")
     end
 
     it "should raise an error if the start is the same as the end date" do
       expect { Availability.available?(start_date: "05/01/2021",
                                      end_date: "05/01/2021",
-                                     listing_id: 1)
+                                     listing_id: listing_id)
       }.to raise_error(AvailabilityError, "Must stay for at least one night")
+    end
+  end
+
+  describe ".confirm_request" do
+    let(:request) { double(:request,
+                           start: Date.parse("03/01/2021"),
+                           end: Date.parse("07/01/2021"),
+                           id: 1,
+                           listing_id: 1,
+                           rejected: false)
+    }
+
+    it "should check availability of the request" do
+      allow(request).to receive(:start).and_return(Date.parse("31/12/2020"))
+      
+      expect { Availability.confirm_request(request, request_class) }.to raise_error(AvailabilityError, "Cannot confirm unavailable request")
+    end
+
+    it "should confirm the request" do
+      expect(request_class).to receive(:update).with(request.id, accepted: true)
+        
+      Availability.confirm_request(request, request_class)
+    end
+
+    it "should split the availability" do
+      Availability.confirm_request(request, request_class)
+
+      availabilities = Availability.all
+
+      expect(availabilities[0].start).to eq(Date.parse("01/01/2021"))
+      expect(availabilities[0].end).to eq(Date.parse("03/01/2021"))
+
+      expect(availabilities[1].start).to eq(Date.parse("07/01/2021"))
+      expect(availabilities[1].end).to eq(Date.parse("31/01/2021"))
+    end
+
+    it "should update the availability if the request is at the start" do
+      allow(request).to receive(:start).and_return(Date.parse("01/01/2021"))
+
+      Availability.confirm_request(request, request_class)
+
+      availabilities = Availability.all
+
+      expect(availabilities.length).to eq(1)
+      expect(availabilities[0].start).to eq(Date.parse("07/01/2021"))
+      expect(availabilities[0].end).to eq(Date.parse("31/01/2021"))
+    end
+
+    it "should update the availability if the request is at the end" do
+      allow(request).to receive(:end).and_return(Date.parse("31/01/2021"))
+
+      Availability.confirm_request(request, request_class)
+
+      availabilities = Availability.all
+
+      expect(availabilities.length).to eq(1)
+      expect(availabilities[0].start).to eq(Date.parse("01/01/2021"))
+      expect(availabilities[0].end).to eq(Date.parse("03/01/2021"))
+    end
+
+    it "should delete the availability if the request fills the available dates" do
+      allow(request).to receive(:start).and_return(Date.parse("01/01/2021"))
+      allow(request).to receive(:end).and_return(Date.parse("31/01/2021"))
+
+      Availability.confirm_request(request, request_class)
+
+      availabilities = Availability.all
+
+      expect(availabilities.length).to eq(0)
+    end
+
+    it "should set coinciding requests to rejected" do
+      expect(request_class).to receive(:coinciding_requests).with(request)
+      # Request should receive update once to accept the request and then
+      # three more times to reject coinciding requests
+      expect(request_class).to receive(:update).exactly(4).times
+
+      Availability.confirm_request(request, request_class)
+    end
+
+    it "should raise an error if the request has already been rejected" do
+      allow(request).to receive(:rejected).and_return(true)
+      
+      expect { Availability.confirm_request(request, request_class) }.to raise_error(AvailabilityError, "Cannot confirm rejected request")
     end
   end
 end
