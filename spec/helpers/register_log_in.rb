185ef04fc0fb7d85aa01dd262eb0f9146a2dc2a6
Jamie95187 <jamiewong63@googlemail.com>
2019-12-19 17:55:42 +0000

    increase pool size for database and changed route in app

--- spec/helpers/register_log_in.rb
+++ spec/helpers/register_log_in.rb
@@ -1,9 +1,9 @@
 def register(name, email, password)
-  visit '/'
+  visit '/users/new'
   fill_in 'name', with: name
   fill_in 'email', with: email
   fill_in 'password', with: password
-  click_button 'submit' 
+  click_button 'submit'
 end
 
 def log_in(email, password)
