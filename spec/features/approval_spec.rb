9b2c25a0b819e3123bc927406a6b57986c9ca30d
Alastair Gilles <agilles91@gmail.com>
2019-12-20 12:04:09 +0100

    Make firefox headless and refactor tests
    A headless firefox webdriver using selenium has been added to capybara
    in the spec helper. For tests that require the use of javascript add the
    line:
    
    Capybara.current_driver = Capybara.javascript_driver
    
    before the test.
    
    Test have also been refactored to avoid repeatedly testing the same
    thing.

--- spec/features/approval_spec.rb
+++ spec/features/approval_spec.rb
@@ -1,7 +1,8 @@
 feature 'Requests' do
+  before(:each) { Capybara.current_driver = Capybara.javascript_driver }
   scenario 'Lodger can see requests they have made' do
     register('Landlord', 'landlord@email.com', 'password123')
-    list_a_space("Test Listing 1", "I am a test description of test listing 1", 1000, "01/01/2020", "07/01/2020")
+    list_a_space("Test Listing 1", "I am a test description of test listing 1", 1000, Time.new(2020, 01, 01), Time.new(2020, 01, 31))
     click_on 'Log out'
     register('Lodger', 'lodger@email.com', 'password456')
     click_button 'request_listing'
@@ -19,18 +20,11 @@ feature 'Requests' do
   end
 
   scenario 'User can see requests they have received and can approve them' do
-    Capybara.current_driver = :selenium
     approval_setup
 
     log_in('landlord@email.com', 'password123')
     click_link 'user_requests'
 
-    expect(page).to have_content("Requests I have received")
-    expect(page).to have_content("Name of listing: Test Listing 1")
-    expect(page).to have_content("Name of user: Lodger")
-    expect(page).to have_content("Email of user: lodger@email.com")
-    expect(page).to have_content("Start date: 2020-01-04")
-    expect(page).to have_content("End date: 2020-01-11")
     expect(page).to have_content("Accept")
     expect(page).to have_content("Reject")
 
@@ -48,16 +42,14 @@ feature 'Requests' do
     expect(page).to have_content('Accepted')
     expect(page).not_to have_content('Pending')
     expect(page).not_to have_content('Rejected')
-
-    Capybara.use_default_driver
   end
 
   scenario 'User can see requests they have received and can reject them' do
-    Capybara.current_driver = :selenium
     approval_setup
 
     log_in('landlord@email.com', 'password123')
     click_link 'user_requests'
+
     click_button 'Reject'
 
     expect(page).to have_content('Rejected')
@@ -72,22 +64,12 @@ feature 'Requests' do
     expect(page).to have_content('Rejected')
     expect(page).not_to have_content('Pending')
     expect(page).not_to have_content('Accepted')
-
-    Capybara.use_default_driver
   end
 
   scenario 'User can see requests they have received' do
-    register('Landlord', 'landlord@email.com', 'password123')
-    list_a_space("Test Listing 1", "I am a test description of test listing 1", 1000, "01/01/2020", "07/01/2020")
-    click_on 'Log out'
-    register('Lodger', 'lodger@email.com', 'password456')
-    click_button 'request_listing'
-    fill_in 'start_date', with: "04/01/2020"
-    fill_in 'end_date', with: "11/01/2020"
-    click_button 'confirm_request'
-    click_on 'Log out'
-    log_in('landlord@email.com', 'password123')
+    approval_setup
 
+    log_in('landlord@email.com', 'password123')
     click_link 'user_requests'
 
     expect(page).to have_content("Requests I have received")
